a82a1a8e2a0aad9a29edc5949ffd45047d49bed0
TheYargonaut <mason1552@live.com>
2020-08-11 01:00:24 -0500

    add partitions with stack graphs and pie charts

--- Group.py
+++ Group.py
@@ -1,5 +1,7 @@
-import os, yaml
+import os, yaml, pdb
+import pandas as pd
 from functools import reduce
+from Ledger import knownFmt
 
 # basic event grouping
 
@@ -17,22 +19,23 @@ class Group( object ):
         self.negate = negate
     
     def applyWhitelist( self, df ):
-        return df[ matchAny( self.whitelist, df ) ]
+        return matchAny( self.whitelist, df )
     
     def applyBlacklist( self, df ):
-        return df[ ~matchAny( self.blacklist, df ) ]
+        return ~matchAny( self.blacklist, df )
 
     def filter( self, df ):
         '''returns filtered pandas dataframe
         first applies whitelist, then blacklist'''
+        f = df[ 'comment' ].str.contains( '.*' )
         if self.whitelist:
-            df = self.applyWhitelist( df )
+            f &= self.applyWhitelist( df )
         if self.blacklist:
-            df = self.applyBlacklist( df )
-        return df
+            f &= self.applyBlacklist( df )
+        return f
     
     def plotDelta( self, df, ax ):
-        df = self.filter( df )
+        df = df[ self.filter( df ) ]
         if self.negate:
             df[ self.title ] = -df[ 'delta' ]
         else:
@@ -40,18 +43,83 @@ class Group( object ):
         df.plot( x='date', y=self.title, ax=ax )
     
     def plotCumulative( self, df, ax ):
-        df = self.filter( df )
+        df = df[ self.filter( df ) ]
         if self.negate:
             df[ self.title ] = -df[ 'delta' ].cumsum()
         else:
             df[ self.title ] = df[ 'delta' ].cumsum()
         df.plot( x='date', y=self.title, ax=ax, drawstyle="steps-post" )
+
     def keys( self ):
         return 'title', 'whitelist', 'blacklist', 'negate'
     def __getitem__( self, key ):
         return getattr( self, key )
 
-# TODO: prioritized partition
+class Partition( object ):
+    def __init__( self, groups=[], blacklist=[], negate=False ):
+        'negate applies to the other group'
+        self.groups = groups
+        self.blacklist = blacklist
+        self.negate = negate
+    
+    def filter( self, df, edges=True ):
+        'split into g+1 groups and return as dictionary'
+        if self.blacklist:
+            df = df[ ~matchAny( self.blacklist, df ) ]
+        early = pd.DataFrame( dict( date=df[ 'date' ].min(),
+                                    delta=0,
+                                    comment='' ), index=[ 0 ] )
+        late = pd.DataFrame( dict( date=df[ 'date' ].max(),
+                                   delta=0,
+                                   comment='' ), index=[ 0 ] )
+        r = {}
+        for g in self.groups:
+            f = g.filter( df )
+            if edges:
+                r[ g.title ] = pd.concat( [ early, df[ f ], late ] ).reset_index( drop=True )
+            else:
+                r[ g.title ] = df[ f ]
+            if g.negate:
+                r[ g.title ][ 'delta' ] = -r[ g.title ][ 'delta' ]
+            df = df[ ~f ]
+        if edges:
+            r[ 'other' ] = pd.concat( [ early, df, late ] ).reset_index( drop=True )
+        else:
+            r[ 'other' ] = df
+        if self.negate:
+            r[ 'other' ][ 'delta' ] = -r[ 'other' ][ 'delta' ]
+        return r
+    
+    def plotDelta( self, df, ax ):
+        for title, data in self.filter( df, False ).items():
+            data[ title ] = data[ 'delta' ]
+            data.plot( x='date', y=title, ax=ax )
+
+    def plotCumulative( self, df, ax ):
+        for title, data in self.filter( df ).items():
+            data[ title ] = data[ 'delta' ].cumsum()
+            data.plot( x='date', y=title, ax=ax, drawstyle='steps-post' )
+
+    def plotLayer( self, stack, layer, title, ax ):
+        if stack is None:
+            stack = layer
+        else:
+            stack = pd.concat( [ stack, layer ] ).sort_values( 'date' ).reset_index( drop=True )
+        stack[ title ] = stack[ 'delta' ].cumsum()
+        stack.plot( x='date', y=title, ax=ax, drawstyle='steps-post' )
+        return stack[ knownFmt[ 'internal' ] ]
+
+    def plotStack( self, df, ax ):
+        data = self.filter( df )
+        stack = None
+        for g in self.groups:
+            stack = self.plotLayer( stack, data[ g.title ], g.title, ax )
+        self.plotLayer( stack, data[ 'other' ], 'other', ax )
+    
+    def plotPie( self, df, ax ):
+        total = { title: data[ 'delta' ].sum() for title, data in self.filter( df ).items() }
+        pd.Series( total ).sort_values().plot.pie( ax=ax )
+        
 
 # file operations
 
--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-from Group import load as loadGroups
+from Group import load as loadGroups, Partition
 from Ledger import loadLedger
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -7,11 +7,17 @@ import pdb
 
 # import all the data
 ledger = loadLedger()
+groups = loadGroups()
+
+expensePart = Partition( [ g for g in groups if g.negate ], sum( [
+    g.whitelist + g.blacklist for g in groups if not g.negate
+], [] ), negate=True )
 
 # make an overall graph
 pd.plotting.register_matplotlib_converters()
 plt.figure()
 ax = plt.gca()
-for g in loadGroups():
-    g.plotCumulative( ledger, ax )
+expensePart.plotPie( ledger, ax )
+# for g in loadGroups():
+#     g.plotCumulative( ledger, ax )
 plt.show()
